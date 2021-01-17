ade834610e403249170d2e956688622e76fb624f
Nikhita Singh <nikhitasingh39@gmail.com>
2021-01-17 08:55:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+ Address address new Address();
+ System.out.print(adress());  
+  }
+}
+ 
+public class Address{
+  Address(){
+    String str = "hii I'm a";
+    return str;
+  }
+}
+
+public class Contact{
+
+}
+public class Flight{
+
+}
+public class Passenger{
+
+}
+public class RegularTicket{
+
+}
+public class Ticket{
+
+}
+public class TouristTicket{
+
+}
